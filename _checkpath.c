74e2278f2c5cb478e6c4d4c3ff230d76356b2e3e
codemayor19 <codemayor19@gmail.com>
2023-11-11 04:01:45 -0800

    new commit

---
+++ _checkpath.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * check_path - check if string has"/bin
+ * @str: the string to check
+ * Return: 0 if false or 1 if true
+ */
+int check_path(char *str)
+{
+	char *bin_str = "/bin/", *pt, *tmp;
+	int i, j;
+
+	pt = malloc(sizeof(char *) * 50);
+	if (pt == NULL)
+		return (0);
+	i = 0;
+	j = 0;
+	while (bin_str[i] != '\0')
+	{
+		if (bin_str[i] != str[i])
+		{
+			free(pt);
+			return (0);
+		}
+		i++;
+	}
+	while (str[i] != '\0')
+	{
+		pt[j] = str[i];
+		i++;
+		j++;
+	}
+	pt[j] = '\0';
+	tmp = check_file(pt);
+	if (tmp != NULL)
+	{
+		free(pt);
+		return (1);
+	}
+	return (0);
+}
